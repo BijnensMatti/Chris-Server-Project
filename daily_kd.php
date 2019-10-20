67bf53200107de1493df8116880a6359d5606e4e
christiankrug <mrminifreddy@gmail.com>
2019-10-20 13:27:58 +0200

    Return error if trying to request casual

--- daily_kd.php
+++ daily_kd.php
@@ -29,14 +29,7 @@ $winsy = $obj[1]["ranked_wins"];
 $losst = $obj[0]["ranked_losses"];
 $lossy = $obj[1]["ranked_losses"];
 } else if ($mode=="casual"){
-$killst = $obj[0]["casual_kills"] ;
-$deathst = $obj[0]["casual_deaths"];
-$killsy = $obj[1]["casual_kills_y"];
-$deathsy = $obj[1]["casual_deaths_y"]; 
-$winst = $obj[0]["casual_wins"];
-$winsy = $obj[1]["casual_wins"];
-$losst = $obj[0]["casual_losses"];
-$lossy = $obj[1]["casual_losses"];
+$error = true;
 }
 $todaywins = $winst-$winsy;
 $todaylosses = $losst-$lossy;
